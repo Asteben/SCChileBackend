bb61e7177607da3abfc98f41eed7d72aa26dcfa5
Asteben <esteban.leon@mail.udp.cl>
2021-11-12 20:38:26 -0300

    Add telefono in get Alarmas

--- routes/alarmas.js
+++ routes/alarmas.js
@@ -43,7 +43,7 @@ router.post("/crearAlarma", async (req, res) => {
 });
 
 router.get("/getAlarmas", async (req, res) => {
-    pool.query('SELECT alarma.* FROM alarma, vecino WHERE alarma.vecino_idvecino = vecino.idvecino AND (alarma.estado = ($1) OR alarma.estado = ($2)) ORDER BY alarma.idalarma', ['activa', 'confirmada'],async (err, rows) => {
+    pool.query('SELECT alarma.*, vecino.telefono FROM alarma, vecino WHERE alarma.vecino_idvecino = vecino.idvecino AND (alarma.estado = ($1) OR alarma.estado = ($2)) ORDER BY alarma.idalarma', ['activa', 'confirmada'],async (err, rows) => {
         if (!err) {
             res.send({
                 code: 200,
@@ -63,7 +63,7 @@ router.get("/getAlarmas", async (req, res) => {
 });
 
 router.get("/getHistAlarm", async (req, res) => {
-    pool.query('SELECT alarma.* FROM alarma, vecino WHERE alarma.vecino_idvecino = vecino.idvecino AND (alarma.estado = ($1)) ORDER BY alarma.idalarma', ['terminada'],async (err, rows) => {
+    pool.query('SELECT alarma.*, vecino.telefono FROM alarma, vecino WHERE alarma.vecino_idvecino = vecino.idvecino AND (alarma.estado = ($1)) ORDER BY alarma.idalarma', ['terminada'],async (err, rows) => {
         if (!err) {
             res.send({
                 code: 200,
